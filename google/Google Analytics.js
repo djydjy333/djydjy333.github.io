f8c1bba735335d29ec9c222532d391099f31ab12
deng <dengjunyang316@gmail.com>
2024-05-27 10:56:24 +0900

    Add files via upload

---
+++ google/Google Analytics.js
@@ -0,0 +1,9 @@
+ <!-- Google Analytics -->
+ <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HEZ4FVQ40E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-HEZ4FVQ40E');
+</script> 
