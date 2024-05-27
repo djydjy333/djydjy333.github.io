0a57b5ab3496fdecb49bb5658c4239351d5078c1
deng <dengjunyang316@gmail.com>
2024-05-27 11:26:49 +0900

    Update Google tag manager body.js

--- google/Google tag manager body.js
+++ google/Google tag manager body.js
@@ -1,4 +1,12 @@
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZ83KTSP"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+(function() {
+  var noscript = document.createElement('noscript');
+  var iframe = document.createElement('iframe');
+  iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-KZ83KTSP";
+  iframe.height = "0";
+  iframe.width = "0";
+  iframe.style.display = "none";
+  iframe.style.visibility = "hidden";
+  noscript.appendChild(iframe);
+
+  document.body.appendChild(noscript);
+})();
