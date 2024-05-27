02f567cc609c3ec937977c94dd4852747cc2c673
deng <dengjunyang316@gmail.com>
2024-05-27 11:25:30 +0900

    Update Google Analytics.js

--- google/Google Analytics.js
+++ google/Google Analytics.js
@@ -1,9 +1,14 @@
- <!-- Google Analytics -->
- <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HEZ4FVQ40E"></script>
-<script>
+(function() {
+  var script = document.createElement('script');
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-HEZ4FVQ40E";
+  script.async = true;
+  document.head.appendChild(script);
+
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  window.gtag = gtag;
   gtag('js', new Date());
   gtag('config', 'G-HEZ4FVQ40E');
-</script> 
+})();
